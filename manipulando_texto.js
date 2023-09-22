3ea5436b859cfece82623c62c5e3e19b06b312da
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-22 11:51:17 -0300

    Adicionando arquivo de manipulação de texto e tipos de dados

---
+++ manipulando_texto.js
@@ -0,0 +1,21 @@
+let numFloat = 123.543;
+let linguagens = ["Java", "C#", "Python"];
+//                  0       1       2
+let txt = "Estão chegando as provas";
+
+// Convertendo a variavel 
+
+    // .toString() - Função nativa do JS. Converte um número em script
+
+        // console.log(typeof numFloat.toString());
+
+        // Forma atualizada de converter 
+            // console.log(typeof String(numFloat));
+
+// A variavel linguagens é um array, um objeto
+
+    // console.log(typeof linguagens);
+    // console.log(linguagens[2]);
+
+// .replace() - Função nativa que altera valores nas strings
+console.log(txt.replace("provas", "avaliações"));
---
+++ tipos_dados.js
@@ -0,0 +1,18 @@
+// LET significa que é possivel alterar o valor diferente do const que é constante
+
+// Comentário CTRL + k + c
+
+// Números inteiros
+let idadeAluno = 18;
+
+// Números flutuantes
+let valorProduto = 5.35;
+
+// String
+let nomeAluno = "Pedro Henrique";
+
+// Boolean
+let casado = true;
+
+// typeof identifica o tipo da variavel referenciada
+console.log(typeof idadeAluno);
