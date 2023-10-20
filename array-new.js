4adc5ff2e45d0583cafcad69bcd141a79b9aa13a
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-20 11:45:34 -0300

    Sundindo arquivo com exercício de for e arquivo com explicação de novos comandos array

---
+++ array-new.js
@@ -0,0 +1,9 @@
+const times = ["Vasco", "Botafogo", "Flamento"]
+
+// .push() -> adicionanddo elementos no array
+times.push("Bangu")
+
+console.log(times)
+
+// Para pesquisar comando 
+// comando javascript mds
---
+++ exercicio.js
@@ -0,0 +1,12 @@
+// Escreva um loop que exiba a frase abaixo no console 
+// "Está é a X° vez que está frase é exibida no console"
+//      - Substitua o "x" pela informação correta;
+//      - A cada execução do loop, incrementar x em +1 até 10
+//      - O loop deve parar quando x for igual ou superior a 10. Exibindo 10x o console
+
+
+for (x=0 ; x<10; x++) {
+
+    console.log(`Está é a ${x + 1}º que está frase é exibida no console.`)
+
+}
