8ada941a492c5de3114f36e98c1aa15c530fa6bf
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-20 11:18:02 -0300

    Adicionando explicação do while

---
+++ while.js
@@ -0,0 +1,16 @@
+const frutas = ["maça", "banana", "uva", "pera"];
+let cont = 0;
+
+// Exemplo 1
+
+    console.log("Exemplo 1 \n")
+
+    //  length trás o tamanho da lista
+
+    while(cont < frutas.length) {
+        
+        // $ - identifica uma variável
+        console.log(`${cont} - ${frutas[cont]}`)
+        cont++
+
+    }
