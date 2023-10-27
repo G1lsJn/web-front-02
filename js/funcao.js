94ae08f1ee301d22d5346ad060d9aa5044c6628d
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-27 10:53:03 -0300

    adicionando introdução de funções

---
+++ js/funcao.js
@@ -0,0 +1,20 @@
+function teste(){
+    console.log("Oláaaa")
+}
+
+function meuNome(name){
+    console.log(`Olá ${name}`)
+}
+
+function calc(arg1, arg2){
+    return arg1 + arg2
+}
+
+console.log(calc(4, 6))
+console.log(calc(2, 3))
+console.log(calc(7, 8))
+
+meuNome("João")
+meuNome("Ana")
+meuNome("Maria")
+
